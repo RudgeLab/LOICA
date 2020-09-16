4385482f2ae82cdd50c44e41a4224f1911911df7
Tim Rudge <timrudge@gmail.com>
2020-09-16 07:56:58 -0300

    Remove networkx and use regulator objects as tu inputs/outputs

--- loica/genetic_network.py
+++ loica/genetic_network.py
@@ -1,23 +1,20 @@
-import networkx as nx
-
-class GeneticNetwork(nx.DiGraph):
+class GeneticNetwork():
     def __init__(self):
-        super().__init__()
+        self.tus = []
+        self.regulators = []
 
     def add_tu(self, tu):
-        self.add_node(tu)
+        self.tus.append(tu)
 
-    def add_link(self, tu1, tu2, regulator):
-        self.add_edge(tu1, tu2, object=regulator)
+    def add_regulator(self, reg):
+        self.regulators.append(reg)
 
     def step(self, growth_rate=1, dt=0.1):
-        for tu in self.nodes:
-            inputs = {reg.name:reg.concentration for tu1,tu2,reg in self.in_edges(tu, data='object')}
-            expression_rate = tu.output(inputs, dt)
-            for tu1,tu2,regulator in self.out_edges(tu, data='object'):
-                regulator.express(expression_rate)
+        for tu in self.tus:
+            expression_rate = tu.expression_rate()
+            tu.output.express(expression_rate)
 
-        for tu1,tu2,regulator in self.edges.data('object'):
+        for regulator in self.regulators:
             regulator.step(growth_rate, dt)
 
 
--- loica/tus.py
+++ loica/tus.py
@@ -5,15 +5,16 @@ class TU:
         return 0
 
 class RepressedRepressor(TU):
-    def __init__(self, input, a, b, K, n):
+    def __init__(self, input, output, a, b, K, n):
         self.a = a
         self.b = b
         self.K = K
         self.n = n
         self.input = input
+        self.output = output
         
-    def output(self, inputs, dt):
-        input_repressor = inputs[self.input]
+    def expression_rate(self):
+        input_repressor = self.input.concentration
         r = (input_repressor/self.K)**self.n
         expression_rate = ( self.a + self.b*r ) / (1 + r)
         return expression_rate
