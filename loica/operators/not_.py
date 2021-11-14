e39a4d3263803afb7e778b70e4b13fc2198210b0
Tim Rudge <timrudge@gmail.com>
2021-11-14 16:21:56 +0000

    Characterization of not operator without profile and without growth profile (direct from biomass)

--- loica/operators/not_.py
+++ loica/operators/not_.py
@@ -7,7 +7,7 @@ from .receiver import *
 class Not:
     color = 'skyblue'
     shape = 's'
-    def __init__(self, input, output, a, b, K, n, profile=None, uri=None, sbol_comp=None):
+    def __init__(self, input, output, a, b, K, n, uri=None, sbol_comp=None):
         self.a = a
         self.b = b
         self.K = K
@@ -16,10 +16,6 @@ class Not:
         self.output = output
         self.uri = uri
         self.sbol_comp = sbol_comp
-        if not profile:
-            def profile(t):
-                return 1
-        self.profile = profile
 
     def __str__(self):
         return 'NOT'
@@ -27,35 +23,12 @@ class Not:
     def expression_rate(self, t, dt):
         input_repressor = self.input.concentration
         r = (input_repressor/self.K)**self.n
-        expression_rate = self.profile(t) * ( self.a + self.b*r ) / (1 + r)
+        expression_rate = ( self.a + self.b*r ) / (1 + r)
         return expression_rate
-
-    def forward_model_growth(
-        self,
-        Dt=0.05,
-        sim_steps=10,
-        muval=[0]*100,
-        od0=0,
-        nt=100
-    ):
-        od_list, t_list = [],[]
-        od = od0
-        for t in range(nt):
-            od_list.append(od)
-            t_list.append([t * Dt])
-            mu = muval[t]
-            for tt in range(sim_steps):
-                doddt = mu * od
-                nextod = od + doddt * Dt/sim_steps
-                od = nextod
-
-
-        aod = np.array(od_list).transpose()
-        tt = np.array(t_list).transpose()
-        return aod,tt
 
     def forward_model(
         self,
+        a_j,
         b_j,
         n_i=2,
         K_i=1,
@@ -66,9 +39,7 @@ class Not:
         Dt=0.05,
         sim_steps=10,
         A=[0],
-        muval=[1]*100, odval=[1]*100,
-        profile_j=[1]*100,
-        profile_A=[1]*100,
+        odval=[1]*100,
         gamma=0,
         p0_1=0, p0_2=0,
         nt=100
@@ -81,15 +52,12 @@ class Not:
             A_list.append(A)
             t_list.append([t * Dt]*len(A))
             od = odval[t]
-            mu = muval[t]
-            prof_A = profile_A(t * Dt)
-            prof_j = profile_j[t]
             for tt in range(sim_steps):
                 time = (t + tt/sim_steps) * Dt
                 a = (A/K_A)**n_A
-                nextp1 = p1 + (prof_A * (a_A + b_A * a) /(1 + a) - gamma*p1 - mu*p1) * Dt/sim_steps
-                p = (p1/K_i)**n_i
-                nextp2 = p2 + ( od * prof_j * (1 + b_j*p) / ( 1 + p )) * Dt/sim_steps
+                nextp1 = p1 + (od * (a_A + b_A * a) /(1 + a) - gamma*p1) * Dt/sim_steps
+                p = (p1/od/K_i)**n_i
+                nextp2 = p2 + ( od * (a_j + b_j*p) / ( 1 + p )) * Dt/sim_steps
                 p1,p2 = nextp1,nextp2
 
         ap2 = np.array(p2_list).transpose()
@@ -98,47 +66,16 @@ class Not:
         t = np.arange(nt) * Dt
         return ap2,AA,tt
 
-    def residuals_growth(self, data, epsilon, dt, t, n_gaussians): 
-        def func(x): 
-            od0 = x[0]
-            muval = np.zeros_like(t)
-            means = np.linspace(t.min(), t.max(), n_gaussians)
-            vars = [(t.max()-t.min())/n_gaussians] * n_gaussians 
-            heights = x[1:]
-            for mean,var,height in zip(means, vars, heights):
-                gaussian = height * np.exp(-(t-mean)*(t-mean) / var / 2) / np.sqrt(2 * np.pi * var)
-                muval += gaussian
-
-            od,tt = self.forward_model_growth(
-                        Dt=dt,
-                        muval=muval,
-                        od0=od0,
-                        nt=len(t)
-                    )
-            model = od
-            residual = (data[1:] - model[1:]) / tt.ravel()[1:]
-            tikhonov = heights
-            result = np.concatenate((residual, epsilon * tikhonov))
-            return result
-        return func
-
-    def residuals(self, data, p0_1, p0_2, profile_A, a_A, b_A, K_A, n_A, A, muval, odval, epsilon, gamma, Dt, t, n_gaussians): 
+    def residuals(self, data, p0_1, p0_2, a_A, b_A, K_A, n_A, A, odval, gamma, Dt, t): 
         def func(x): 
             nt = len(t)
             n_i = x[0]
-            K_i = x[1]
-            #gamma = x[2]
-            #b_j = x[2]
-            profile_j = np.zeros_like(t)
-            means = np.linspace(t.min(), t.max(), n_gaussians)
-            vars = [(t.max()-t.min())/n_gaussians] * n_gaussians 
-            heights = x[2:]
-            for mean,var,height in zip(means, vars, heights):
-                gaussian = height * np.exp(-(t-mean)*(t-mean) / var / 2) / np.sqrt(2 * np.pi * var)
-                profile_j += gaussian
-            
+            K_i = x[1]  
+            a_j = x[2]          
+            b_j = x[3]         
             p,AA,tt = self.forward_model(
-                        b_j=0,
+                        a_j=a_j,
+                        b_j=b_j,
                         n_i=n_i,
                         K_i=K_i,
                         a_A=a_A,
@@ -147,64 +84,15 @@ class Not:
                         n_A=n_A,
                         Dt=Dt,
                         A=A,
-                        muval=muval, odval=odval,
-                        profile_j=profile_j,
-                        profile_A=profile_A,
+                        odval=odval,
                         gamma=gamma,
                         nt=nt,
                         p0_1=p0_1, p0_2=p0_2
                     )
             model = p.ravel()
             residual = (data[1:] - model[1:]) 
-            tikhonov = heights
-            result = np.concatenate((residual, epsilon * tikhonov))
-            return result
+            return residual
         return func
-
-    def characterize_growth(self,
-            flapjack, 
-            inverter, 
-            media, 
-            strain, 
-            biomass_signal, 
-            n_gaussians, 
-            epsilon
-            ):
-        # Characterize growth rate profile
-        biomass = flapjack.analysis(type='Background Correct', 
-                            vector=inverter,
-                            media=media,
-                            strain=strain,
-                            signal=biomass_signal,
-                            biomass_signal=biomass_signal
-                         )
-        biomass = biomass.sort_values('Time')
-        t = biomass.Time.unique()
-        dt = np.mean(np.diff(t))
-        nt = len(t)
-
-        biomass = biomass.groupby('Time').mean().Measurement.values
-        lower_bounds = [0] + [0]*n_gaussians
-        upper_bounds = [1] + [50]*n_gaussians
-        bounds = [lower_bounds, upper_bounds]
-
-        data = biomass
-        res = least_squares(
-                self.residuals_growth(data, epsilon=epsilon, dt=dt, t=t, n_gaussians=n_gaussians), 
-                [0.01] + [1]*n_gaussians, 
-                bounds=bounds
-                )
-        self.init_biomass = res.x[0]
-        profile = np.zeros_like(t)
-        means = np.linspace(t.min(), t.max(), n_gaussians)
-        vars = [(t.max()-t.min())/n_gaussians] * n_gaussians 
-        heights = res.x[1:]
-        for mean,var,height in zip(means, vars, heights):
-            gaussian = height * np.exp(-(t-mean)*(t-mean) / var / 2) / np.sqrt(2 * np.pi * var)
-            profile += gaussian
-        self.mu_profile = profile
-        self.biomass = biomass
-
 
     def characterize(self, 
             flapjack, 
@@ -213,20 +101,20 @@ class Not:
             media, 
             strain, 
             signal, 
-            biomass_signal, 
-            n_gaussians, 
-            epsilon,
+            biomass_signal,
             gamma
             ):
-        # Get growth rate profile
-        self.characterize_growth(flapjack, 
-            inverter, 
-            media, 
-            strain, 
-            biomass_signal, 
-            n_gaussians, 
-            epsilon)
-
+        # Get biomass time series
+        biomass = flapjack.analysis(type='Background Correct', 
+                            vector=inverter,
+                            media=media,
+                            strain=strain,
+                            signal=biomass_signal,
+                            biomass_signal=biomass_signal
+                         )
+        biomass = biomass.sort_values('Time')
+        biomass = biomass.groupby('Time').mean().Measurement.values
+        
         # Characterize receiver profile and Hill function
         rec = Receiver(None, None, 0, 0, 0, 0)
         rec.characterize(
@@ -235,13 +123,10 @@ class Not:
             media=media,
             strain=strain,
             signal=signal,
-            biomass_signal=biomass_signal,
-            n_gaussians=n_gaussians,
-            epsilon=epsilon
+            biomass_signal=biomass_signal
         )
         self.a_A = rec.a
         self.b_A = rec.b
-        self.profile_A = rec.profile
         self.K_A = rec.K
         self.n_A = rec.n
 
@@ -262,43 +147,29 @@ class Not:
         inverter = inverter.groupby(['Concentration1', 'Time']).mean().Measurement.values
 
         # Bounds for fitting
-        lower_bounds = [0]*2 + [0]*n_gaussians
-        upper_bounds = [4, 1e8]  + [1e10]*n_gaussians
+        lower_bounds = [0]*4
+        upper_bounds = [8, 1e8, 1e8, 1e8]
         bounds = [lower_bounds, upper_bounds]
 
         '''
             n_i = x[0]
             K_i = x[1]
-            gamma = x[2]
+            a_j = x[2]
             b_j = x[3]
-            profile_j = x[4:]
         '''
         # Solve for parameters and profile
         data = inverter.ravel()
         residuals = self.residuals(
                                 data, 
-                                0, data[0],
-                                self.profile_A, 
+                                0, data[0], 
                                 self.a_A, self.b_A, self.K_A, self.n_A, A,
-                                self.mu_profile, self.biomass, 
+                                biomass, 
                                 gamma=gamma,
-                                epsilon=0, Dt=dt, t=t,
-                                n_gaussians=n_gaussians
+                                Dt=dt, t=t
                             )
-        res = least_squares(residuals, [1,1] + [1]*n_gaussians, bounds=bounds)
+        res = least_squares(residuals, [1,1,1,0], bounds=bounds)
         self.res = res
         self.n = res.x[0]
         self.K = res.x[1]
-        #self.gamma = res.x[2]
-        #self.b = res.x[2]
-
-        profile = np.zeros_like(t)
-        means = np.linspace(t.min(), t.max(), n_gaussians)
-        vars = [(t.max()-t.min())/n_gaussians] * n_gaussians 
-        heights = res.x[2:]
-        for mean,var,height in zip(means, vars, heights):
-            gaussian = height * np.exp(-(t-mean)*(t-mean) / var / 2) / np.sqrt(2 * np.pi * var)
-            profile += gaussian
-
-        self.a = profile.max()
-        self.profile = interp1d(t, profile/self.a, fill_value='extrapolate', bounds_error=False)
+        self.a = res.x[2]
+        self.b = res.x[3]
