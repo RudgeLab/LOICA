4a630ff09940097f7f8bf1dde6f7a769d742343a
Gonza10V <gsvidal@uc.cl>
2021-11-16 23:01:36 -0300

    to_sbol function now incorporates PRO and RNA inputs and outputs

--- loica/genetic_network.py
+++ loica/genetic_network.py
@@ -1,6 +1,11 @@
 import sbol3
 import networkx as nx
 from .geneproduct import Regulator, Reporter
+from .supplement import Supplement
+from .operators.not_ import Not
+from .operators.nor import Nor
+import tyto
+from typing import List #, Dict, Tuple, Optional, Union, Any
 
 class GeneticNetwork():
     def __init__(self, vector=None):
@@ -98,9 +103,11 @@ class GeneticNetwork():
             font_weight=font_weight
             )
 
-    def to_sbol(self):
+    def to_sbol(self, sbol_doc=None):
         # prototype for not gate interaction
-        doc = sbol3.Document()
+        if sbol_doc:
+            doc=sbol_doc
+        else: doc = sbol3.Document()
         geneticnetwork = sbol3.Component('geneticnetwork', sbol3.SBO_DNA)
         geneticnetwork.roles.append(sbol3.SO_ENGINEERED_REGION)
         loica_set = set()
@@ -108,14 +115,10 @@ class GeneticNetwork():
             operator_comp = op.sbol_comp
             output_comp = op.output.sbol_comp
             input_comp = op.input.sbol_comp
-            print(f'Input: {input_comp}, Operator {operator_comp}, Output:{output_comp}')
+            #print(f'Input: {input_comp}, Operator {operator_comp}, Output:{output_comp}')
             operator_sc = sbol3.SubComponent(operator_comp)
             output_sc = sbol3.SubComponent(output_comp)
             input_sc = sbol3.SubComponent(input_comp)
-            #if not in doc
-            loica_set.add(operator_comp)
-            loica_set.add(output_comp)
-            loica_set.add(input_comp)
             # TU Component
             tu = sbol3.Component(f'TU_{op.input.name}_{op}_{op.output.name}', sbol3.SBO_DNA) #generalize to multi input/output TUs
             tu.roles.append(sbol3.SO_ENGINEERED_REGION)
@@ -126,44 +129,91 @@ class GeneticNetwork():
             # if they are the same then delete one of them and concatenate the rest.
             # else error or comment TU sequence can not be generated, provide ways to add it.
 
-            # Protein Component
-            protein = sbol3.Component(f'protein_{op.input.name}_{op}_{op.output.name}', sbol3.SBO_PROTEIN)
-            protein.roles.append(sbol3.SO_TRANSCRIPTION_FACTOR)
-            protein_sc = sbol3.SubComponent(protein)
-            tu.features.append(protein_sc)
+            # Output GeneProduct Component
+            if type(op.output)==Regulator:
+                if op.output.type_ == 'PRO':
+                    output_gp_comp = sbol3.Component(f'{op.output.name}_protein', sbol3.SBO_PROTEIN)
+                    output_gp_comp.roles.append(sbol3.SO_TRANSCRIPTION_FACTOR)               
+                elif op.output.type_ == 'RNA':
+                    output_gp_comp = sbol3.Component(f'{op.output.name}_rna', sbol3.SBO_RNA)
+                    output_gp_comp.roles.append(sbol3.SO_TRANSCRIPTION_FACTOR)
+                else: 
+                    print('Unsupported output molecule type')
+            elif type(op.output)==Reporter: # For now just support fluorescent reporters
+                if op.output.type_ == 'PRO':
+                    output_gp_comp = sbol3.Component(f'{op.output.name}_protein', sbol3.SBO_PROTEIN)
+                    output_gp_comp.roles.append('http://purl.obolibrary.org/obo/NCIT_C37894')
+                elif op.output.type_ == 'RNA':
+                    output_gp_comp = sbol3.Component(f'{op.output.name}_rna', sbol3.SBO_RNA)
+                    output_gp_comp.roles.append('http://purl.obolibrary.org/obo/NCIT_C37894')  
+                else: 
+                        print('Unsupported output molecule type')
+            else:
+                print('Unsupported output Type')
+            output_gp_sc = sbol3.SubComponent(output_gp_comp)
+            tu.features.append(output_gp_sc)
+            loica_set.add(output_gp_comp)
             # obtain TU subcomponents sequences, specially CDS and flanking parts sequences
             # look for ATG on the CDS and upstream part sequences (in the case of MoClo the ATG is in the fusion sites)
             # look for stop codons on frame with the ATG.
             # add translated the sequence between the ATG and the stop codon as protein sequence.
             #protein.sequence = tu.cds.sequence
 
-            # Production Interaction (change TU for CDS)
+            # Input Product Component
+            if op.input != List:
+                inputs = [op.input]
+            else: inputs = op.input
+            inputs_prod_sc = []
+            for op_input in inputs:
+                if type(op_input)==Regulator:
+                    if op_input.type_ == 'PRO':
+                        input_prod_comp = sbol3.Component(f'{op_input.name}_protein', sbol3.SBO_PROTEIN)
+                        input_prod_comp.roles.append(sbol3.SO_TRANSCRIPTION_FACTOR)               
+                    elif op_input.type_ == 'RNA':
+                        input_prod_comp = sbol3.Component(f'{op_input.name}_rna', sbol3.SBO_RNA)
+                        input_prod_comp.roles.append(sbol3.SO_TRANSCRIPTION_FACTOR)
+                    else: 
+                        print('Unsupported input molecule type')
+                elif type(op_input)==Supplement:
+                    input_prod_comp = sbol3.Component(f'{op_input.name}_chemical', sbol3.SBO_SIMPLE_CHEMICAL)
+                    input_prod_comp.roles.append(sbol3.SO_TRANSCRIPTION_FACTOR)     
+                else:
+                    print('Unsupported input Type')
+                input_prod_sc = sbol3.SubComponent(input_prod_comp)
+                tu.features.append(input_prod_sc)
+                inputs_prod_sc.append(input_prod_sc)
+                loica_set.add(input_prod_comp)
+
+            # Genetic Production Interaction pf the output
             output_participation = sbol3.Participation(roles=[sbol3.SBO_TEMPLATE], participant=output_sc)
-            protein_participation = sbol3.Participation(roles=[sbol3.SBO_PRODUCT], participant=protein_sc)
-            production = sbol3.Interaction(types=[sbol3.SBO_GENETIC_PRODUCTION], participations=[output_participation, protein_participation])
+            gp_participation = sbol3.Participation(roles=[sbol3.SBO_PRODUCT], participant=output_gp_sc)
+            production = sbol3.Interaction(types=[sbol3.SBO_GENETIC_PRODUCTION], participations=[output_participation, gp_participation])
             tu.interactions.append(production)
             # Inhibition Interaction
-            input_participation = sbol3.Participation(roles=[sbol3.SBO_INHIBITOR], participant=input_sc)
-            op_participation = sbol3.Participation(roles=[sbol3.SBO_INHIBITED], participant=operator_sc)
-            interaction = sbol3.Interaction(types=[sbol3.SBO_INHIBITION], participations=[input_participation, op_participation])
-            tu.interactions.append(interaction)
+            if type(op)==Not or Nor:
+                for input_prod_sc in inputs_prod_sc:
+                    input_participation = sbol3.Participation(roles=[sbol3.SBO_INHIBITOR], participant=input_prod_sc)
+                    op_participation = sbol3.Participation(roles=[sbol3.SBO_INHIBITED], participant=operator_sc)
+                    interaction = sbol3.Interaction(types=[sbol3.SBO_INHIBITION], participations=[input_participation, op_participation])
+                    tu.interactions.append(interaction)
+            else:
+                print('Unsupported input Type')
+            
             # Model
-            #op_model_attachment = sbol3.Attachment()
+            # op_model_attachment = sbol3.Attachment()
             op_model = sbol3.Model(f'LOICA_{op.input.name}_{op}_{op.output.name}_model', 
                             source='https://github.com/SynBioUC/LOICA/blob/master/loica/operators/not_.py',
-                            language='http://identifiers.org/EDAM:format_3996', # http://edamontology.org/format_3996
-                            framework='http://identifiers.org/SBO:0000062' #,attachments=[op]
-                            )
+                            language='http://identifiers.org/EDAM:format_3996',
+                            framework='http://identifiers.org/SBO:0000062' )#,attachments=[op])
             doc.add(op_model)
             tu.models.append(op_model)
-            #tu.derived_from = []
             doc.add(tu)
-            tu_sc = sbol3.SubComponent(tu)#(instance_of=tu.identity )
+            tu_sc = sbol3.SubComponent(tu)
             geneticnetwork.features.append(tu_sc)
         loica_list = list(loica_set)
-        #doc.add(loica_list) # waiting release
-        for obj in loica_list:
-            doc.add(obj)
+        doc.add(loica_list) 
+        #for obj in loica_list:
+        #    doc.add(obj)
         if len(geneticnetwork.features) > 1:
             for i in range(len(geneticnetwork.features)-1):
                 geneticnetwork.constraints = [sbol3.Constraint(sbol3.SBOL_PRECEDES, geneticnetwork.features[i], geneticnetwork.features[i+1])]
