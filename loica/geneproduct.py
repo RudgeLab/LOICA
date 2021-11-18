bf7ee72eeec6d35aa1bc3f0d3f34903c30741ce3
Gonza10V <gsvidal@uc.cl>
2021-11-18 13:32:56 -0300

    First docstrings added to test automated documentation

--- loica/geneproduct.py
+++ loica/geneproduct.py
@@ -1,8 +1,18 @@
 import numpy as np
 
 class GeneProduct:
+    """Representation of a gene product."""
     shape = '^'
     def __init__(self, name, init_concentration=0, degradation_rate=0, uri=None, sbol_comp=None, type_='PRO'):
+        """Initialize a gene product.
+
+        :param name: Name of the gene product.
+        :param init_concentration: Initial concentration of the gene product.
+        :param degradation_rate: Degradation rate of the gene product.
+        :param uri:  SynBioHub URI of the gene product.
+        :param sbol_comp: SBOL Component vinculated to the gene product.
+        :param type_: Molecular type of the gene product, cpuld be 'PRO' or 'RNA'.
+        """
         self.init_concentration = init_concentration
         self.concentration = init_concentration
         self.degradation_rate = degradation_rate
@@ -10,7 +20,7 @@ class GeneProduct:
         self.expression_rate = 0
         self.uri = uri
         self.sbol_comp = sbol_comp
-        self.type_ = type_ # 'PRO' or 'RNA'
+        self.type_ = type_ 
 
 
     def initialize(self):
@@ -29,13 +39,20 @@ class GeneProduct:
         return self.name
 
 class Regulator(GeneProduct):
+    """Representation of a regulatory gene product."""
     color = 'lightgreen'
     def __init__(self, name, init_concentration=0, degradation_rate=0, sbol_comp=None):
         super().__init__(name, init_concentration, degradation_rate)
         self.sbol_comp = sbol_comp
 
 class Reporter(GeneProduct):
+    """Representation of a regulatory gene product."""
     def __init__(self, name, init_concentration=0, degradation_rate=0, signal_id=None, color='w', sbol_comp=None):
+        """Initialize Reporter
+        
+        :param signal_id: Flapjack ID of the signal that the reporter is associated with.
+        :param color: Color of the reporter.
+        """
         super().__init__(name, init_concentration, degradation_rate, sbol_comp=None)
         self.signal_id = signal_id
         self.color = color
