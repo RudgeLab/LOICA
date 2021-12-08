c19c830e63b44b7796cb955ba51603913e5e8990
Gonzalo Vidal <35148159+Gonza10V@users.noreply.github.com>
2021-12-08 19:37:50 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,38 @@
-import os
-#from distutils.core import setup
 from setuptools import setup
-import subprocess
+
+
+with open('README.md', 'r') as ld:
+    long_description = ld.read()
 
 setup(name='loica',
+    version='1.0.0',
+    author='Gonzalo Vidal',
+    author_email='gsvidal@uc.cl',
+    description='LOICA: Logical Operators for Integrated Cell Algorithms',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/RudgeLab/LOICA',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
     install_requires=[        
         'numpy', 
 	'networkx',
 	'pandas',
-    'sbol3',
-    'tyto',
-    'tdqm'
+    	'sbol3',
+    	'tyto',
+   	'tdqm'
         ],
     setup_requires=[
         'numpy',
 	'networkx',
 	'pandas',
-    'sbol3',
-    'tyto',
-    'tdqm'
+    	'sbol3',
+    	'tyto',
+    	'tdqm'
         ],
     packages=['loica'],
-    python_requires='>=3',
-    version='v0.0'
+    python_requires='>=3'
 )
