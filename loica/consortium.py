36dd8ac4ed053acc743c46f0caa9e1be2108df55
LKonkina <l.konkina2@newcastle.ac.uk>
2022-05-18 16:49:10 +0100

    Create consortium.py and describe class Consortium

---
+++ loica/consortium.py
@@ -0,0 +1,8 @@
+class Consortium:
+    """
+    Representation of microbial consortium (written either to substitute Sample or 
+    is a child of sample). Consortium contains Strains, which are connected through 
+    Medium.
+    
+    
+    """
