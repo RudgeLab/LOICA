95cf0294ff8d5c37560491e88f2629aa99fbf04b
LKonkina <l.konkina2@newcastle.ac.uk>
2022-05-27 11:33:09 +0100

    Delete old sample.py, rename sample2.py to sample.py

--- loica/sample.py
+++ loica/sample.py
@@ -7,9 +7,9 @@ class Sample:
 
     Attributes
     ----------
-    genetic_network : GeneticNetwork
+    genetic_network : List[GeneticNetwork] or GeneticNetwork
         genetic network that is part of the sample
-    metabolism : Metabolism
+    metabolism : list[Metabolism] or Metabolism
         metabolism that drives the genetic network in the sample
     assay : Assay
         assay to which this sample belongs
@@ -23,6 +23,7 @@ class Sample:
     add_supplement(supplement, concentration)
         stablishes the concentration of Supplement
     """
+    # TODO: determine whether I need different metabolism for each strain (in general)
     def __init__(self, 
             genetic_network=None, 
             metabolism=None, 
@@ -35,17 +36,44 @@ class Sample:
         self.media = media
         self.strain = strain
         self.vector = self.genetic_network.vector
-        if self.genetic_network:
+        self.reporters = []
+
+        # adding all reporters into list
+        if type(self.genetic_network)==list:
+            for genetic_network in self.genetic_network:
+                for reporter in genetic_network.reporters:
+                    self.reporters.append(reporter)
+        else:
             self.reporters = self.genetic_network.reporters
-        if metabolism:
-            self.biomass = self.metabolism.biomass
+
+        if self.metabolism:
+            if type(self.metabolism)==list:
+                self.biomass = []
+                for m in metabolism:
+                    self.biomass.append(m.biomass)
+            else:       
+                self.biomass = self.metabolism.biomass
+
         self.supplements = {}
 
     def initialize(self):
-        self.genetic_network.initialize()
+        if type(self.genetic_network)==list:
+            for gn in self.genetic_network:
+                gn.initialize()
+        else:
+            self.genetic_network.initialize()
+        # and initialise extracellular concentrations if needed
 
     def set_supplement(self, supplement, concentration):
         self.supplements[supplement] = concentration
+
+    # add function that checks whether supplement is the same as gene product
+    # if yes, add the supplement concentration to the concentration of gp in the
+    # extracellular space
+    def supplement_is_gp(self, supplement):
+        for gp in self.extracellular_space:
+            if supplement.name == gp.name:
+                gp.extracellular_conc=supplement.concentration
 
     def set_regulator(self, name, concentration):
         for reg in self.genetic_network.regulators:
@@ -61,13 +89,43 @@ class Sample:
 
     def step(self, t, dt, stochastic=False):
         if self.genetic_network and self.metabolism:
-            growth_rate = self.metabolism.growth_rate(t)
+            # check if metabolism is list
+            if type(self.metabolism)==list:
+                growth_rate = []
+                biomass = []
+                for m in self.metabolism:
+                    growth_rate.append(m.growth_rate(t))
+                    biomass.append(m.biomass(t))
+            else:
+                growth_rate = self.metabolism.growth_rate(t)
+                biomass = self.metabolism.biomass(t)
+
             for supp,conc in self.supplements.items():
                 supp.concentration = conc
+
             if stochastic:
-                self.genetic_network.step_stochastic(growth_rate, t, dt)
+                # I need for all strains to have steps either simultaneously or
+                # randomly, with the simultaneous change extracellularly
+                if type(self.genetic_network)==list:
+                    for gn in self.genetic_network:
+                        gn.step_stochastic(growth_rate, t, dt)
+                else:
+                    self.genetic_network.step_stochastic(growth_rate, t, dt)
             else:
-                self.genetic_network.step(growth_rate, t, dt)
-            self.reporters = self.genetic_network.reporters
+                if type(self.genetic_network)==list and type(biomass)==list:
+                    for (gn, b) in zip(self.genetic_network, biomass):
+                        gn.step(b, growth_rate, t, dt)
+                elif type(self.genetic_network)==list:
+                    for gn in self.genetic_network:
+                        gn.step(biomass, growth_rate, t, dt) 
+                else:
+                    self.genetic_network.step(biomass, growth_rate, t, dt)
+            
+            if type(self.genetic_network)==list:
+                for gn in self.genetic_network:
+                    for reporter in gn.reporters:
+                        self.reporters.append(reporter)
+            else:
+                self.reporters = self.genetic_network.reporters
 
 
