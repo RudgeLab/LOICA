4c6772cb6bc4a11908680ba5c5f65356064a0006
LKonkina <l.konkina2@newcastle.ac.uk>
2022-05-27 12:21:06 +0100

    Fix inheritance of diffusion_rate

--- loica/geneproduct.py
+++ loica/geneproduct.py
@@ -74,8 +74,8 @@ class Regulator(GeneProduct):
     Representation of a regulatory gene product.
     Child of GeneProduct.
     """
-    def __init__(self, name, init_concentration=0, degradation_rate=0, sbol_comp=None, color='lightgreen'):
-        super().__init__(name, init_concentration, degradation_rate, sbol_comp,color='lightgreen')
+    def __init__(self, name, init_concentration=0, degradation_rate=0, diffusion_rate=0, sbol_comp=None, color='lightgreen'):
+        super().__init__(name, init_concentration, degradation_rate, diffusion_rate, sbol_comp,color='lightgreen')
         self.sbol_comp = sbol_comp
 
 class Reporter(GeneProduct):
@@ -89,8 +89,8 @@ class Reporter(GeneProduct):
     color : str, optional
         Color of the reporter
     """
-    def __init__(self, name, init_concentration=0, degradation_rate=0, signal_id=None, color='w', sbol_comp=None):
-        super().__init__(name, init_concentration, degradation_rate, sbol_comp)
+    def __init__(self, name, init_concentration=0, degradation_rate=0, diffusion_rate=0, signal_id=None, color='w', sbol_comp=None):
+        super().__init__(name, init_concentration, degradation_rate, diffusion_rate, sbol_comp)
         self.signal_id = signal_id
         self.color = color
         self.sbol_comp = sbol_comp
