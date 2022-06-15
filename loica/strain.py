6dd12e6eabf0e52a4fe830f7bd9e4daf941db64e
LKonkina <l.konkina2@newcastle.ac.uk>
2022-06-15 17:45:55 +0100

    Create class Strain

---
+++ loica/strain.py
@@ -0,0 +1,27 @@
+class Strain:
+    """
+    Representation of a strain that encapsulates GeneticNetwork and Metabolism.
+    ...
+
+    Attributes
+    ----------
+    genetic_network : GeneticNetwork
+        genetic network that is part of the strain
+    metabolism : Metabolism
+        metabolism that drives the genetic network in the strain
+    
+    """
+
+    def __init__(self, genetic_network=None, metabolism=None):
+        self.genetic_network = genetic_network
+        self.metabolism = metabolism
+        if self.genetic_network:
+            self.reporters = self.genetic_network.reporters
+            self.gene_products = self.genetic_network.regulators + self.reporters
+        if metabolism:
+            self.biomass = self.metabolism.biomass
+            self.growth_rate = self.metabolism.growth_rate
+    
+        
+
+        
