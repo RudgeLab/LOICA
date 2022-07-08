ee3cee4a9f2e71dc697964c7ac957a36a5335fe3
LKonkina <l.konkina2@newcastle.ac.uk>
2022-07-08 19:54:24 +0100

    Fix degradation rate and ES concentration, kinetic parameters can be lists

--- loica/operators/degrader.py
+++ loica/operators/degrader.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.optimize import least_squares
 from .receiver import *
 
-class Degrader(Enzyme):
+class Degrader(Operator):
     """
     A class that represents an enzyme that degrades a substrate (or substrates).
     ...
@@ -29,19 +29,31 @@ class Degrader(Enzyme):
 
     def __init__(self, input, output, k1, k1r, k2, name=None, uri=None, sbol_comp=None, color='skyblue'):
         super().__init__(output, name, uri, sbol_comp, color)
-        self.k1 = k1
-        self.k1r = k1r
-        self.k2 = k2
         self.input = input
+
+        if type(k1)==list:
+            self.k1 = k1
+        else:
+            self.k1 = [k1]
+
+        if type(k1r)==list:
+            self.k1r = k1r
+        else:
+            self.k1r = [k1r]
+
+        if type(k2)==list:
+            self.k2 = k2
+        else:
+            self.k2 = [k2]
+
         if type(output)==list:
             self.output = output
         else:
             self.output = [output]
+
         self.es_complex = []
         for o in self.output:
             self.es_complex.append(0)
-        
-       
 
     def __str__(self):
         if self.name == None:
@@ -53,18 +65,21 @@ class Degrader(Enzyme):
         enzyme = []
         
         for i, output in enumerate(self.output):
-
+            # test
+            print(f'{(self.input.concentration / len(self.output) - self.es_complex[i])} split enzyme conc')
             # if enzyme has multiple substrates, enzyme is split equally between all substrates (account for enzymes in complex)
-            self.enzyme.append(self.input.concentration / len(self.output) - self.es_complex[i])
+            enzyme.append(self.input.concentration / len(self.output) - self.es_complex[i])
 
             # calculate substrate degradation rate
-            degr_rate = -self.k1 * enzyme[i] * output.concentration + self.k1r * self.es_complex[i] - self.k2 * self.es_complex[i]
-            degradation_rate.append(degr_rate)
+            substrate_change_rate = -self.k1 * enzyme[i] * output.concentration + self.k1r * self.es_complex[i] - self.k2 * self.es_complex[i]
+            # test
+            print(f'degr_rate is {substrate_change_rate}')
+            degradation_rate.append(-substrate_change_rate)
 
             # update enzyme-substrate complex concentrations
             es_change = self.k1 * enzyme[i] * output.concentration - self.k1r * self.es_complex[i] - self.k2 * self.es_complex[i]
             # enzyme_change = -self.k1 * enzyme[i] * output.concentration + self.k1r * self.es_complex[i] + self.k2 * self.es_complex[i]
-            new_es = es_change * dt
+            new_es = self.es_complex[i] + es_change * dt
             self.es_complex[i] = new_es
 
         return degradation_rate
