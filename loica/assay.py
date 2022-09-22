a023f27d5411736323264be6af053984c5431726
LKonkina <l.konkina2@newcastle.ac.uk>
2022-09-22 12:13:11 +0100

    Edit description, delete unused code

--- loica/assay.py
+++ loica/assay.py
@@ -35,7 +35,7 @@ class Assay:
         (empty) - gives overall fluorescence and biomass measurements
         'track all' - raw numbers from the simulation. Records all GeneProducts in each
                       strain and extracellular space. Also records biomass of each strain.
-        'single_cell' - fluorescence measurements of a cell per strain
+        'single_cell' - fluorescence measurements of a single cell per strain
     upload(flapjack, study)
         Upload the data produced by running the Assay to Flapjack into the Study
     """
@@ -65,23 +65,21 @@ class Assay:
         '''
         Run the assay measuring at specified time points, with simulation time step dt
         '''
-        #substeps = self.interval / dt
         dt = self.interval / substeps
         n_samples = len(self.samples)
         with tqdm(total=100) as pbar:
             for sample_id, sample in enumerate(self.samples):
                 sample.initialize()
                 # Calibrate sample so cell number could be calculated from absorbance
+                # assumes that all strains have equal ppod
                 if ppod:
                     sample.calibrate(ppod)
                 # Integrate models
                 for t in range(self.n_measurements):
-                    # print(f'Current t={t}')
                     time = t * self.interval
                     if mode == 'track_all':
-                    # Record raw measurements of each gene product both intracellularly 
+                    # Record raw measurements of each GeneProduct both intracellularly 
                     # and extracellularly
-                        # for tt in range(substeps):
                         for group in sample.gene_products:
                             row = {
                                     'Time': time, 
@@ -122,8 +120,6 @@ class Assay:
                                 'Sample':sample_id
                                 }
                         self.measurements = self.measurements.append(row, ignore_index=True)
-                            # time = t * self.interval + tt * dt
-                            # sample.step(time, dt)
 
                     elif mode=='single_cell':
                         for strain in sample.strain:
@@ -183,6 +179,7 @@ class Assay:
                                 'Sample':sample_id
                                 }
                         self.measurements = self.measurements.append(row, ignore_index=True)
+                   
                     # Compute next time step
                     for tt in range(substeps):
                         time = t * self.interval + tt * dt
