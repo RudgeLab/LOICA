4d85ec59cfa12241cba2e03592a6e3d940717a9a
Tim Rudge <timrudge@gmail.com>
2023-07-06 19:51:28 +0100

    Draft of fitting with profile, does not fit well

--- loica/operators/hill1.py
+++ loica/operators/hill1.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy.optimize import least_squares
 from scipy.integrate import solve_ivp, odeint
 from scipy.interpolate import interp1d
+import matplotlib.pyplot as plt
+from flapjack import *
 from .receiver import *
 
 class Hill1(Operator):
@@ -64,63 +66,65 @@ class Hill1(Operator):
         b_j,
         n_i,
         K_i,
-        a_A,
-        b_A,
-        K_A,
-        n_A,
-        A,
         od,
+        profile,
+        rec_profile,
         gamma,
         p0_1, p0_2,
-        tt
+        tt,
+        sim_steps=10
     ):
-
-        # od = callable OD as function of time
-        #def dydt(od, a_A, b_A, K_A, n_A, a_j, b_j, K_i, n_i):
+        # od, rec_profile = callable as function of time
         def dydt(y, t):
-            p1,p2 = y
-            a = (A/K_A)**n_A
-            p = (p1/od(t)/K_i)**n_i
-            dp1dt = od(t) * (a_A + b_A * a) /(1 + a) - gamma*p1
-            dp2dt = od(t) * (a_j + b_j*p) / ( 1 + p )
-            return np.array([dp1dt,dp2dt])
+            #a = (A/K_A)**n_A
+            #p = (p1/od(t)/K_i)**n_i
+            #dp1dt = od(t) * (a_A + b_A * a) /(1 + a) - gamma*p1
+            rec = max(rec_profile(t), 1)
+            odval = max(od(t), 0.001)
+            p1 = rec / odval
+            p = (p1 / K_i)**n_i
+            dp2dt = odval * profile(t) * (a_j + b_j*p) / ( 1 + p )
+            return dp2dt
         #res = solve_ivp(fun=dydt, t_span=[0, np.max(tt)], y0=np.array([p0_1, p0_2], dtype=float), t_eval = tt)
         #y = res.sol(tt)
-        y = odeint(dydt, np.array([p0_1, p0_2]), tt)
-        p2 = y[:,1]
+        y = odeint(dydt, p0_2, tt, rtol=1e-3)
+        p2 = y[:,0]
 
         '''
         p2_list,A_list,t_list = [],[],[]
-        p1 = p0_1     
         p2 = p0_2
-        for t in tt:
-            od = odval[t]
-            #od = max(od, 1e-3)
-            if od>=0:
+        p2_list.append(p2)
+        t_list.append(tt[0])
+        nt = len(tt)
+        for it in range(1, nt):
+            t = tt[it]
+            Dt = t - tt[it-1]
+            if od(t)>0:
                 p2_list.append(p2)
-                A_list.append(A)
-                t_list.append(t * Dt)
-                for tt in range(sim_steps):
-                    time = (t + tt/sim_steps) * Dt
-                    a = (A/K_A)**n_A
-                    nextp1 = p1 + (od * (a_A + b_A * a) /(1 + a) - gamma*p1) * Dt/sim_steps
-                    p = (p1/od/K_i)**n_i
-                    nextp2 = p2 + ( od * (a_j + b_j*p) / ( 1 + p )) * Dt/sim_steps
-                    p1,p2 = nextp1,nextp2
+                t_list.append(t)
+                for st in range(sim_steps):
+                    time = (t + st/sim_steps) * Dt
+                    rec = max(rec_profile(time), 1)            
+                    p1 = rec / od(time)
+                    p = (p1/K_i)**n_i
+                    nextp2 = p2 + ( od(time) * (a_j + b_j*p) / ( 1 + p )) * Dt/sim_steps
+                    p2 = nextp2
 
-        ap2 = np.array(p2_list).transpose()
+        p2 = np.array(p2_list).transpose()
         AA = np.array(A_list).transpose()
         tt = np.array(t_list).transpose()
         t = np.arange(nt) * Dt
         '''
         return p2,tt
 
-    def residuals(self, df, oddf, rec_df, gamma): 
+    def residuals(self, df, oddf, rec_df, profile, gamma, plotting=False): 
         def func(x): 
-            b_j = np.exp(x[3])
-            a_j = b_j/ np.exp(x[2]) 
-            n_i = np.exp(x[0])
-            K_i = np.exp(x[1])  
+            self.data = []
+            self.model = []
+            b_j = x[3] #np.exp(x[3])
+            a_j = x[2] #b_j / np.exp(x[2]) 
+            n_i = x[0] #np.exp(x[0])
+            K_i = x[1] #np.exp(x[1])  
             print(a_j, b_j, K_i, n_i)
             
             residual_array = np.array((0,))
@@ -137,30 +141,40 @@ class Hill1(Operator):
                 p0_1 = 0
                 p0_2 = samp_data.Measurement.values[0]
                 A = samp_data.Concentration1.values[0]
-                rec_profile = rec_df[rec_df.Concentration1==A].sort_values('Time').groupby('Time').mean().Rate.values
-                rec_profile_t = rec_df[rec_df.Concentration1==A].sort_values('Time').groupby('Time').mean().index
-                rec_profile = interp1d(rec_profile_t, rec_profile, bounds_error=False, fill_value='extrapolate')
-                if np.isnan(A):
-                    A = 0
-                p,tt = self.forward_model(
-                            a_j=a_j,
-                            b_j=b_j,
-                            n_i=n_i,
-                            K_i=K_i,
-                            a_A=a_A,
-                            b_A=b_A,
-                            K_A=K_A,
-                            n_A=n_A,
-                            A=A,
-                            od=od,
-                            gamma=gamma,
-                            p0_1=p0_1, p0_2=p0_2,
-                            tt=tt
-                        )
-                data = samp_data.Measurement.values
-                model = p.ravel()
-                residual_array = np.append(residual_array, data[1:] - model[1:])
-            print(np.sum(np.sqrt(residual_array * residual_array)))
+                #if np.isnan(A):
+                #    A = 0
+                #    rec_profile = rec_df[rec_df.Concentration1.isna()].sort_values('Time').groupby('Time').mean().Measurement.values
+                #    rec_profile_t = rec_df[rec_df.Concentration1.isna()].sort_values('Time').groupby('Time').mean().index
+                #else:
+                if ~np.isnan(A):
+                    rec_profile = rec_df[rec_df.Concentration1==A].sort_values('Time').groupby('Time').mean().Measurement.values
+                    rec_profile_t = rec_df[rec_df.Concentration1==A].sort_values('Time').groupby('Time').mean().index
+                    #print(A)
+                    #print(rec_profile_t)
+                    #print(rec_profile)
+                    rec_profile = interp1d(rec_profile_t[rec_profile>0], rec_profile[rec_profile>0], bounds_error=False, fill_value='extrapolate')
+                    p,tt = self.forward_model(
+                                a_j=a_j,
+                                b_j=b_j,
+                                n_i=n_i,
+                                K_i=K_i,
+                                od=od,
+                                profile=profile,
+                                rec_profile=rec_profile,
+                                gamma=gamma,
+                                p0_1=p0_1, p0_2=p0_2,
+                                tt=tt
+                            )
+                    data = samp_data.Measurement.values
+                    self.data.append(data)
+                    model = p.ravel()
+                    self.model.append(model)
+
+                    if plotting:
+                        plt.plot(tt, model, '--')
+                        plt.plot(tt, data)
+                    residual_array = np.append(residual_array, data[1:] - model[1:])
+            print(np.sum(residual_array * residual_array))
             return residual_array.ravel()
         return func
 
@@ -168,6 +182,7 @@ class Hill1(Operator):
             flapjack, 
             receiver, 
             inverter, 
+            study,
             media, 
             strain, 
             signal, 
@@ -176,6 +191,7 @@ class Hill1(Operator):
             ):
         # Get biomass time series
         biomass_df = flapjack.analysis(type='Background Correct', 
+                            study=study,
                             vector=inverter,
                             media=media,
                             strain=strain,
@@ -202,20 +218,29 @@ class Hill1(Operator):
         '''
         
         rec_df = flapjack.analysis(vector=receiver,
+                            study=study,
                             signal=signal,
                             media=media,
                             strain=strain,
-                            type='Expression Rate (indirect)',
+                            type='Background Correct',
                             biomass_signal=biomass_signal)
 
-        # Characterize inverter
-        inverter_df = flapjack.analysis(type='Background Correct', 
-                            vector=inverter,
+        inv_df = flapjack.analysis(vector=inverter,
+                            study=study,
+                            signal=signal,
                             media=media,
                             strain=strain,
-                            signal=signal,
-                            biomass_signal=biomass_signal
-                         )
+                            type='Background Correct',
+                            biomass_signal=biomass_signal)
+        inv_er_df = expression_rate_inverse(inv_df, biomass_df)
+        inv_er_df = inv_er_df[inv_er_df.Concentration1.isna()]
+        inv_er_val = inv_er_df.groupby('Time').mean().Rate.values
+        inv_er_t = inv_er_df.groupby('Time').mean().index
+        
+        profile = interp1d(inv_er_t, inv_er_val/inv_er_val.max(), bounds_error=False, fill_value='extrapolate')
+
+
+        # Characterize inverter
         '''
             b_j = np.exp(x[3])
             a_j = b_j/ np.exp(x[2]) 
@@ -226,21 +251,34 @@ class Hill1(Operator):
         b = self.alpha[1]
         K = self.K
         n = self.n
-        initx = [np.log(n), np.log(K), np.log(b/a), np.log(b)] 
+        initx = [n, K, a, b] 
+        lower_bounds = [1, 0, 0, 0]
+        upper_bounds = [6, 1e8, 1e8, 1e8]
+        bounds = [lower_bounds, upper_bounds]
 
+        func = self.residuals(inv_df,
+                        biomass_df,
+                        rec_df,
+                        profile,
+                        gamma=gamma,
+                        plotting=False)
         # Solve for parameters
-        print(inverter_df)
-        print(biomass_df)
-        print('Testing func')
-        func = self.residuals(inverter_df,
-                        biomass_df, 
-                        self.a_A, self.b_A, self.K_A, self.n_A,
-                        gamma=gamma)
-        print(f'func(x0) = {func(initx)}')
         res = least_squares(fun=func, 
                             x0=initx,
+                            bounds=bounds,
+                            diff_step=[0.1,0.1,0.1,0.1]
                             #ftol=1e-3
         )
+
+        func = self.residuals(inv_df,
+                        biomass_df,
+                        rec_df,
+                        profile,
+                        gamma=gamma,
+                        plotting=True)
+        func(res.x)
+
+        plt.show()
 
         print(res)
         self.res = res
@@ -248,4 +286,5 @@ class Hill1(Operator):
         self.K = np.exp(res.x[1])
         self.alpha[1] = np.exp(res.x[3])
         self.alpha[0] = self.alpha[1] / np.exp(res.x[2])
+        self.profile = profile
       
